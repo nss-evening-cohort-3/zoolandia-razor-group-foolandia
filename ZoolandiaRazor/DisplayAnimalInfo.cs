53f0daa70ffc654d6a07cf38d05a2748f74a3659
Bernard Anderson <bernardanderson@gmail.com>
2016-10-08 11:32:31 -0500

    Added storage classes for the returned db info

---
+++ ZoolandiaRazor/DisplayAnimalInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ZoolandiaRazor
+{
+    public class DisplayAnimalInfo
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string CommonName { get; set; }
+        public string ScientificName { get; set; }
+        public string CurrentHabitat { get; set; } // Just the habitat name will be inserted
+        public int Age { get; set; }
+    }
+}
---
+++ ZoolandiaRazor/DisplayEmployeeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ZoolandiaRazor
+{
+    public class DisplayEmployeeInfo
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> CurrentAssignedHabitats { get; set; } // List of habitat names
+
+    }
+}
