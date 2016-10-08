e4ea98ca8e71aef1e924e2e9709afcbf0e54cdf0
Sandy Jones <sandynashv@gmail.com>
2016-10-08 10:10:43 -0500

    removed unnecessary usings

--- ZoolandiaRazor/Models/Employee.cs
+++ ZoolandiaRazor/Models/Employee.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace ZoolandiaRazor.Models
 {
