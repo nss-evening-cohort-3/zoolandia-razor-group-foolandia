30e75594b24c70e27ace174c2bbc0e5ceee50e3c
Callan Morrison <callan.l.morrison@gmail.com>
2016-10-08 11:19:49 -0500

    create global test variables

---
+++ ZoolandiaRazor.Tests/DAL.ZoolandiaRepo.Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZoolandiaRazor.DAL;
+using Moq;
+using ZoolandiaRazor.Models;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace ZoolandiaRazor.Tests
+{
+    [TestClass]
+    public class DALZoolandiaRepoTests
+    {
+        Mock<ZoolandiaContext> mock_context { get; set; }
+        
+        //Animals Table and List
+        Mock<DbSet<Animal>> mock_animal_table { get; set; }
+        List<Animal> animals_list { get; set; }
+
+        //Employee Table and List
+        Mock<DbSet<Employee>> mock_employee_table { get; set; }
+        List<Employee> employee_list { get; set; }
+        ZoolandiaRepo repo { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
